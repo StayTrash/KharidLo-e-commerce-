824439b31f3c4592c19d25693622032b65d54b9c
Shubham0-5 <shubham.2022ug2012@iiitranchi.ac.in>
2025-05-15 03:18:16 +0530

    viye config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,21 +8,6 @@
 
 
 
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api/": "http://localhost:5000",
-//       "/uploads/": "http://localhost:5000",
-//     },
-//   },
-// });
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -31,8 +16,23 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://kharid-lo-e-commerce.vercel.app/",
-      "/uploads/": "https://kharid-lo-e-commerce.vercel.app/",
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
 });
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api/": "https://kharid-lo-e-commerce.vercel.app/",
+//       "/uploads/": "https://kharid-lo-e-commerce.vercel.app/",
+//     },
+//   },
+// });
